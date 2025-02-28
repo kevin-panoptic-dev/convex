7aa01e21f12710a37a13d3fc2ce2fc8eaa1a3b57
Aloy's entourage <entourage@fidelity.com>
2025-02-27 22:33:19 -0500

    save

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        outDir: "dist",
+        rollupOptions: {
+            input: {
+                background: path.resolve(__dirname, "src/background.ts"),
+                popup: path.resolve(__dirname, "index.html"),
+            },
+            output: {
+                format: "es",
+                entryFileNames: "[name].js",
+            },
+        },
+    },
+});
